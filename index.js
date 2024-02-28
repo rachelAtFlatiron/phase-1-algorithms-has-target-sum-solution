30cefaa1903f860645bfb4d6b179d83fad3eabe0
Rachel Katz <rachel.katz@flatironschool.com>
2024-02-28 12:42:28 -0500

    commit tree 643071482529cc132f01aff4318e41309af2b0dd

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+// [1, 3, 4, 2, 9, 7]
+// 1 -> 1+3, 1+4, 1+2, 1+9, 1+7
+// 3 -> 3+4, 3+2, 3+9, 3+7
+// 4 -> 4+2, 4+9, 4+7
+// 2 -> 2+9, 2+7
+// 9 -> 9+7
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i = 0; i < array.length - 1; i++){
+    for(let j = i+1; j < array.length; j++){
+      if(array[i] + array[j] === target) {
+        return true
+      }
+    }
+  }
+  return false
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,6 +23,13 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  1. for loop i index
+    (let i = 0; i < array.length - 1; i++)
+    2. for loop j index
+    (let j = i + 1; j < array.length ; j++)
+      3. test if array[i] + array[j] === target
+        4. return true (we don't have to finish looking through the rest of the array)
+  5. return false (we have finished our for loops, we have checked every single possible combination, and found no matching sum)
 */
 
 /*
